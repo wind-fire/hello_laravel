ec38d4c7bc8af65b189e365e74753defbf143da3
wind-fire <877400859@qq.com>
2018-03-29 18:51:56 +0800

    @2018

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 <body>
 <div class="container">
     <div class="content">
-        <div class="title">Hello Laravel! - by Fire</div>
+        <div class="title">Hello Laravel! - by Fire@2018</div>
     </div>
 </div>
 </body>
